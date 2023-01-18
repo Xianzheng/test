700e618c3c8541a8a0686e03e6b5c9a06fd67ce3
xianzheng <mark.ffang@gmail.com>
2023-01-18 01:56:33 -0500

    'addTest'

---
+++ t.py
@@ -0,0 +1,14 @@
+def c1(*a):
+    print(a)
+
+
+def d12(**c):
+    print(c)
+
+
+d12(x = '123',type = 'string' ,vb = '123')
+
+a = 'abcd'
+print(a[:-1])
+import datetime
+print(datetime.datetime.now())
